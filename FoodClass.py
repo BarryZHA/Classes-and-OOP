fdff5f0c0ef175f3de201fcbf8c97b72fa1929f7
Zheng Zhang <Zheng_Zhang1@baylor.edu>
2023-02-16 23:27:23 -0600

    update

--- FoodClass.py
+++ FoodClass.py
@@ -8,18 +8,30 @@
 ## The Transaction class should have accessor methods only for each attribute.
 
 class Customer: 
-    def __int__(self,cust_id,name,address,email,phone,memberstat): 
+
+    def __init__(self,cust_id,name,address,email,phone,memberstat): 
         self.__cust_id = cust_id
         self.__name = name
         self.__address = address
         self.__email = email
         self.__phone = phone
         self.__memberstat = memberstat
-
+        self.__totalcost = 0 
+        self.__memberdiscount = 0 
 
 class Transaction: 
-    def __int__(self,date,item_name,cost,cust_id):
+    def __init__(self,date,item_name,cost,cust_id):
         self.__date = date
         self.__item_name = item_name
         self.__cost = cost
         self.__cust_id = cust_id
+
+    def total_cost(self,transaction,cust_id): 
+        for n in transaction: 
+            match cust_id: 
+                case transaction[n][3]:
+                    self.__totalcost += transaction[n][3]
+    
+    def get_total_cost(self): 
+        return self.__totalcost
+
--- FoodProgram.py
+++ FoodProgram.py
@@ -1,4 +1,19 @@
 import FoodClass as fc
+cust_id = 570
+name = "Danni Sellyar"
+address = "97 Mitchell Way Hewitt Texas 76712"
+email = "dsellyarft@gmpg.org"
+phone = "254-555-9362"
+memberstat = False
+customer1 = fc.Customer(cust_id,name,address,email,phone,memberstat)
+
+# cust_id = "569"
+# name = "Aubree Himsworth"
+# address = "1172 Moulton Hill Waco Texas 76710"
+# email = "ahimsworthfs@list-manage.com"
+# phone = "254-555-2273"
+# memberstat = True
+# customer1 = fc.Customer(cust_id,name,address,email,phone,memberstat)
 
 # this dictionary represents transactions. The key of the dictionary is the transaction identifier.
 # The Value of the dictionary is a list. The elements in each list are -
@@ -9,6 +24,31 @@ dict = {'trans1':['2/15/2023','The Lone Patty',17,569],
                 'trans3':['2/15/2023','The Octoveg',16,570],
                 'trans4':['2/15/2023','The Octoburger',20,570],
 }
+date = '2/15/2023'
+NameOfItem = 'The Lone Patty'
+cost = 17
+customerid = 569 
+trans1 = fc.Transaction(date,NameOfItem,cost,customerid)
+
+date = '2/15/2023'
+NameOfItem = 'The Octobreakfast'
+cost = 18
+customerid = 569 
+trans2 = fc.Transaction(date,NameOfItem,cost,customerid)
+
+date = '2/15/2023'
+NameOfItem = 'The Octoveg'
+cost = 16
+customerid = 570
+trans3 = fc.Transaction(date,NameOfItem,cost,customerid)
+
+date = '2/15/2023'
+NameOfItem = 'The Octoburger'
+cost = 20
+customerid = 570
+trans4 = fc.Transaction(date,NameOfItem,cost,customerid)
+
+fc.Customer.get_transaction(dict,cust_id)
 
 order_total = 0
 
